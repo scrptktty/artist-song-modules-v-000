8607cd8001045e08447f71303eb39662f580e24d
Kachel Wittig <kjwittig@gmail.com>
2018-04-18 18:08:38 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -8,13 +8,13 @@ module Memorable
     @@songs.clear
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-  #
-  # def self.count
-  #   @@artists.count
-  # end
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    @@artists.count
+  end
 
 
 end
