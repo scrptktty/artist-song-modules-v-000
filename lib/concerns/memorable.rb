10c95531bca738a4f939566f6dd7fcb9a4d61945
Kachel Wittig <kjwittig@gmail.com>
2018-04-18 18:51:07 +0000

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,28 @@
+module Memorable
+
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+
+    def find_by_name(name)
+      all.detect{|a| a.name == name}
+    end
+  end
+
+
+  module InstanceMethods
+    def to_param
+      name.downcase.gsub(' ', '-')
+    end
+
+    def initialize
+      self.class.all << self
+    end
+  end
+
+end #module Memorable
