6a6c7ddf2c9f27181a769284e43476dfc63654c4
Kachel Wittig <kjwittig@gmail.com>
2018-04-18 18:26:21 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,7 @@
 module Findable
 
   def find_by_name(name)
-    @artists.detect{|a| a.name == name}
-  end
-
-  def find_by_name(name)
-    @songs.detect{|a| a.name == name}
+    self.floobar.detect{|a| a.name == name}
   end
 
 end
