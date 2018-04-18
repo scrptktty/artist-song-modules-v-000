74962a85eee0dc717771d6d86a0d9fd6ec8b155a
Kachel Wittig <kjwittig@gmail.com>
2018-04-18 18:47:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,5 @@
 require 'pry'
 require_relative 'concerns/memorable.rb'
-require_relative 'concerns/findable.rb'
-require_relative 'concerns/paramable.rb'
 
 class Song
   extend Memorable::ClassMethods
